31be53c085997f8e3e02245bb5794ceb4a41e88d
Hector Clessi <121068920+hectorc2907@users.noreply.github.com>
2024-08-28 12:05:53 -0300

    configuramos el formulario de registro

--- src/components/pages/RegisterForm.jsx
+++ src/components/pages/RegisterForm.jsx
@@ -1,5 +1,43 @@
+import { useForm } from "react-hook-form";
+import { useContext } from "react";
+import AuthContext from "../../context/AuthContext";
+
 const RegisterForm = () => {
-  return <h1>Register</h1>;
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const { register: registerUser } = useContext(AuthContext);
+
+  const onSubmit = async (data) => {
+    try {
+      await registerUser(data.email, data.password);
+    } catch (error) {
+      console.error("Registration failed:", error);
+    }
+  };
+  return (
+    <div>
+      <h1>Register</h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input
+          type="email"
+          {...register("email", { required: "Email is required" })}
+          placeholder="Email"
+        />
+        {errors.email && <p>{errors.email.message}</p>}
+        <input
+          type="password"
+          {...register("password", { required: "Password is required" })}
+          placeholder="Password"
+        />
+        {errors.email && <p>{errors.email.message}</p>}
+
+        <button type="submit">Register</button>
+      </form>
+    </div>
+  );
 };
 
 export default RegisterForm;
