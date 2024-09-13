f2392ffb02420ee52150e0863d0078eb52f90bfc
Hector Clessi <121068920+hectorc2907@users.noreply.github.com>
2024-09-13 13:24:52 -0300

    configuramos el MainLayout con el Header y Footer

--- src/components/layout/MainLayout.jsx
+++ src/components/layout/MainLayout.jsx
@@ -1,7 +1,14 @@
-const MainLayout = () => {
-  return (
-    <div>MainLayout</div>
-  )
-}
+import Footer from "../common/Footer";
+import Header from "../common/Header";
 
-export default MainLayout
+const MainLayout = ({ children }) => {
+  return (
+    <>
+      <Header />
+      <main>{children}</main>
+      <Footer />
+    </>
+  );
+};
+
+export default MainLayout;
