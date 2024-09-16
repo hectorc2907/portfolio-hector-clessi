2e847b6e2dd44f899dc9401a3ef45b4cfcab5982
Hector Clessi <121068920+hectorc2907@users.noreply.github.com>
2024-09-15 22:02:03 -0300

    configuramos los 4 angulos de animaciones

--- src/utils/Animations.jsx
+++ src/utils/Animations.jsx
@@ -14,3 +14,54 @@ export const FaceUp = (delay) => {
     },
   };
 };
+
+export const FaceDown = (delay) => {
+  return {
+    hidden: {
+      opacity: 0,
+      y: -100,
+    },
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        duration: 1,
+        delay: delay,
+      },
+    },
+  };
+};
+
+export const FaceLeft = (delay) => {
+  return {
+    hidden: {
+      opacity: 0,
+      x: 100,
+    },
+    visible: {
+      opacity: 1,
+      x: 0,
+      transition: {
+        duration: 1,
+        delay: delay,
+      },
+    },
+  };
+};
+
+export const FaceRight = (delay) => {
+  return {
+    hidden: {
+      opacity: 0,
+      x: -100,
+    },
+    visible: {
+      opacity: 1,
+      x: 0,
+      transition: {
+        duration: 1,
+        delay: delay,
+      },
+    },
+  };
+};
