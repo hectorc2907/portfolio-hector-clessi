f79c0f0e97b225f295c4f250493fbd1cd42ff795
Hector Clessi <121068920+hectorc2907@users.noreply.github.com>
2024-09-15 23:05:55 -0300

    configuramos animaciones para el footer

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -1,3 +1,4 @@
+import { motion } from "framer-motion";
 import FooterLogo from "../ui/footer/FooterLogo";
 import FooterMenu from "../ui/footer/FooterMenu";
 import FooterCopy from "../ui/footer/FooterCopy";
@@ -7,11 +8,16 @@ const Footer = () => {
   return (
     <>
       <footer className="flex justify-center bg-blue-900 pt-12 mt-12">
-        <div className="container grid grid-cols-1 md:grid-cols-3">
+        <motion.div
+          initial={{ opacity: 0 }}
+          whileInView={{ opacity: 1 }}
+          transition={{ duration: 1, delay: 0.2 }}
+          className="container grid grid-cols-1 md:grid-cols-3"
+        >
           <FooterLogo />
           <FooterMenu />
           <FooterNetwork />
-        </div>
+        </motion.div>
       </footer>
       <FooterCopy />
     </>
--- src/components/ui/footer/FooterCopy.jsx
+++ src/components/ui/footer/FooterCopy.jsx
@@ -1,7 +1,18 @@
-import React from "react";
+import { motion } from "framer-motion";
 
 const FooterCopy = () => {
-  return <p className="bg-blue-900 text-white py-6 text-center">HAC Servicios &copy; Derechos Reservados</p>;
+  return (
+    <div className="bg-blue-900 text-white ">
+      <motion.p
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ duration: 1, delay: 0.2 }}
+        className="py-6 text-center"
+      >
+        HAC Servicios &copy; Derechos Reservados
+      </motion.p>
+    </div>
+  );
 };
 
 export default FooterCopy;
