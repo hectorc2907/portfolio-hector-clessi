f0f6461e080fd96add068964ae9b41e21dbd412b
Hector Clessi <121068920+hectorc2907@users.noreply.github.com>
2024-09-16 15:26:04 -0300

    corregimos un poco el ui del modelo responsive en dispositivos moviles

--- src/components/pages/HomeComponents/works/Projects.jsx
+++ src/components/pages/HomeComponents/works/Projects.jsx
@@ -3,9 +3,12 @@ import CardProjects from "./CardProjects";
 
 const Projects = () => {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-20 py-10">
+    <div className="flex justify-center">
+
+    <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-10 md:gap-20 py-10">
       <CardNewProjects />
       <CardProjects />
+    </div>
     </div>
   );
 };
