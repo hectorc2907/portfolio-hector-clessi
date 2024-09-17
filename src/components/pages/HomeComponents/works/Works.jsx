8add07497c4092472fbd0db2e02ec76717155a4b
Hector Clessi <121068920+hectorc2907@users.noreply.github.com>
2024-09-17 11:59:00 -0300

    configuramos el boton de la seccion de trabajos

--- src/components/pages/HomeComponents/works/Works.jsx
+++ src/components/pages/HomeComponents/works/Works.jsx
@@ -8,14 +8,20 @@ const Works = () => {
       <div className="container">
         <motion.h2
           initial={{ opacity: 0, x: -100 }}
-          whileTap={{ opacity: 1, x: 0 }}
+          animate={{ opacity: 1, x: 0 }}
           transition={{ duration: 1, delay: 0.3 }}
           className="text-center text-4xl italic font-guerrilla"
         >
           Proyectos
         </motion.h2>
         <Projects />
-        <LinkButton direct="/works" text="Ver Mas Proyectos!" />
+        <motion.div
+          initial={{ opacity: 0, x: -100 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ duration: 1, delay: 1.3 }}
+        >
+          <LinkButton direct="/works" text="Ver Mas Proyectos!" />
+        </motion.div>
       </div>
     </div>
   );
