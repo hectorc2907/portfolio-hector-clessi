e7789cb37221f6bac5b6c3b16419de75e5e28647
Hector Clessi <121068920+hectorc2907@users.noreply.github.com>
2024-09-17 17:09:45 -0300

    configuramos los botones de animacion

--- src/components/pages/WorksComponents/WorksPage.jsx
+++ src/components/pages/WorksComponents/WorksPage.jsx
@@ -22,44 +22,57 @@ const WorksPage = () => {
           >
             Mi Camino en el Desarrollo
           </motion.h1>
-          <div className="flex flex-col justify-center items-center gap-5">
-            <motion.h2
-              initial={{ opacity: 0, x: 100 }}
-              animate={{ opacity: 1, x: 0 }}
-              transition={{ duration: 1, delay: 0.5 }}
-              className="text-3xl italic font-guerrilla"
-            >
-              Proyectos
-            </motion.h2>
-            <motion.input
-              initial={{ opacity: 0, x: -100 }}
-              animate={{ opacity: 1, x: 0 }}
-              transition={{ duration: 1, delay: 0.5 }}
-              type="text"
-              value={filterText}
-              onChange={handleInputChange}
-              placeholder="Buscar por tecnologia"
-              className="text-black p-2 rounded-lg"
-            />
-            <motion.div
-              initial={{ opacity: 0, y: 100 }}
-              animate={{ opacity: 1, y: 0 }}
-              transition={{ duration: 1, delay: 0.5 }}
-              className="py-5"
-            >
-              <CardProjectsAll filterText={filterText} />
+          <div>
+            <div className="flex flex-col justify-center items-center gap-5">
+              <motion.h2
+                initial={{ opacity: 0, x: 100 }}
+                animate={{ opacity: 1, x: 0 }}
+                transition={{ duration: 1, delay: 0.5 }}
+                className="text-3xl italic font-guerrilla"
+              >
+                Proyectos
+              </motion.h2>
+              <motion.input
+                initial={{ opacity: 0, x: -100 }}
+                animate={{ opacity: 1, x: 0 }}
+                transition={{ duration: 1, delay: 0.5 }}
+                type="text"
+                value={filterText}
+                onChange={handleInputChange}
+                placeholder="Buscar por tecnologia"
+                className="text-black p-2 rounded-lg"
+              />
+              <motion.div
+                initial={{ opacity: 0, y: 100 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{ duration: 1, delay: 0.5 }}
+                className="py-5"
+              >
+                <CardProjectsAll filterText={filterText} />
+              </motion.div>
+            </div>
+            <motion.div className="flex flex-col gap-5">
+              <motion.p
+                initial={{ opacity: 0, x: 100 }}
+                animate={{ opacity: 1, x: 0 }}
+                transition={{ duration: 1, delay: 1.5 }}
+                className="text-center"
+              >
+                *Por motivos de rendimiento solo se muestran 8 proyectos maximo,
+                puedes buscar por tecnologias o mirar mi{" "}
+                <span className="font-bold">GitHub</span>
+              </motion.p>
+              <motion.div
+                initial={{ opacity: 0, x: -100 }}
+                animate={{ opacity: 1, x: 0 }}
+                transition={{ duration: 1, delay: 1.5 }}
+              >
+                <RedirectButton
+                  direct="https://github.com/hectorc2907?tab=repositories"
+                  text="GitHub"
+                />
+              </motion.div>
             </motion.div>
-          </div>
-          <div className="flex flex-col gap-5">
-            <p className="text-center">
-              *Por motivos de rendimiento solo se muestran 8 proyectos maximo,
-              puedes buscar por tecnologias o mirar mi{" "}
-              <span className="font-bold">GitHub</span>
-            </p>
-            <RedirectButton
-              direct="https://github.com/hectorc2907?tab=repositories"
-              text="GitHub"
-            />
           </div>
         </div>
       </div>
