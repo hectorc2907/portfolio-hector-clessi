3fbe6c6fb00d6ec2c36f141ce4472b86ef63a42d
Hector Clessi <121068920+hectorc2907@users.noreply.github.com>
2024-09-17 12:27:23 -0300

    iniciamos la seccion sobre mi

--- src/components/pages/AboutComponents/AboutMePage.jsx
+++ src/components/pages/AboutComponents/AboutMePage.jsx
@@ -1,7 +1,13 @@
 const AboutMePage = () => {
   return (
-    <div>AboutMePage</div>
-  )
-}
+    <>
+      <div className="flex flex-col items-center">
+        <div className="container">
+          <h1 className="text-center text-white font-roboto text-5xl py-5">¿Quien Soy?</h1>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default AboutMePage
+export default AboutMePage;
