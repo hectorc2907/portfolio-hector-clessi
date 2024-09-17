86da83da76b82b3009dae825e914ca24ff461025
Hector Clessi <121068920+hectorc2907@users.noreply.github.com>
2024-09-17 16:01:45 -0300

    agregamos animaciones a los titulos y comenzamos con la WorksPage

--- src/components/pages/AboutComponents/AboutMePage.jsx
+++ src/components/pages/AboutComponents/AboutMePage.jsx
@@ -1,3 +1,4 @@
+import { motion } from "framer-motion";
 import FirstSection from "./card/FirstSection";
 import SecondSection from "./card/SecondSection";
 import ThirdSection from "./card/ThirdSection";
@@ -6,10 +7,15 @@ const AboutMePage = () => {
   return (
     <>
       <div className="flex flex-col items-center">
-        <div className="container grid grid-cols-1 gap-10">
-          <h1 className="text-center text-white font-roboto text-5xl pt-10">
+        <div className="container grid grid-cols-1 gap-10 md:px-5 xl:px-0">
+          <motion.h1
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 1, delay: 0.5 }}
+            className="text-center text-white font-roboto text-5xl pt-10"
+          >
             ¿Quien Soy?
-          </h1>
+          </motion.h1>
           <div className="flex flex-col gap-10">
             <FirstSection />
             <SecondSection />
--- src/components/pages/WorksComponents/WorksPage.jsx
+++ src/components/pages/WorksComponents/WorksPage.jsx
@@ -1,7 +1,22 @@
+import { motion } from "framer-motion";
+
 const WorksPage = () => {
   return (
-    <div>WorksPage</div>
-  )
-}
+    <>
+      <div className="flex flex-col items-center">
+        <div className="container grid grid-cols-1 gap-10">
+          <motion.h1
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 1, delay: 0.5 }}
+            className="text-center text-white font-roboto text-5xl pt-10"
+          >
+            Mi Camino en el Desarrollo
+          </motion.h1>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default WorksPage
+export default WorksPage;
