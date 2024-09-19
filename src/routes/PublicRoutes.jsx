d29be4f370ccc55034ac645c8561f8d207de25c8
Hector Clessi <121068920+hectorc2907@users.noreply.github.com>
2024-09-19 20:11:24 -0300

    comenzamos con el trabajo de una single page

--- src/routes/PublicRoutes.jsx
+++ src/routes/PublicRoutes.jsx
@@ -1,28 +1,12 @@
-import { Routes, Route, useLocation } from "react-router-dom";
-import { useEffect } from "react";
-import HomePage from "../components/pages/HomeComponents/HomePage";
-import LoginPage from "../components/pages/LoginComponents/LoginPage";
-import AboutMePage from "../components/pages/AboutComponents/AboutMePage";
-import WorksPage from "../components/pages/WorksComponents/WorksPage";
-import ContactPage from "../components/pages/ContactComponents/ContactPage";
-import Error404 from "../components/pages/Error404Components/Error404";
-import Project from "../components/pages/ProjectComponents/Project";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "../pages/HomePage";
+import PageNotFound from "../pages/PageNotFound";
 
 const PublicRoutes = () => {
-  const { pathname } = useLocation();
-
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, [pathname]);
   return (
     <Routes>
       <Route path="/" exact element={<HomePage />} />
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/about" element={<AboutMePage />} />
-      <Route path="/works" element={<WorksPage />} />
-      <Route path="/contact" element={<ContactPage />} />
-      <Route path="/project/:id" element={<Project />} />
-      <Route path="*" element={<Error404 />} />
+      <Route path="*" element={<PageNotFound />} />
     </Routes>
   );
 };
