bd3462c9aefb85e022cf4bcb1c83db55fa041ec3
Hector Clessi <121068920+hectorc2907@users.noreply.github.com>
2024-09-20 13:10:39 -0300

    eliminamos los comentarios

--- src/components/ui/AboutMe/Hero/Hero.jsx
+++ src/components/ui/AboutMe/Hero/Hero.jsx
@@ -8,18 +8,13 @@ const Hero = () => {
     <>
       <section className="px-5 py-10 flex justify-center">
         <div className="container grid grid-cols-1 lg:grid-cols-2 gap-10">
-          {/* Hero Info */}
           <div className="flex justify-center lg:justify-start items-center order-2 lg:order-1">
-            {/* Hero Title */}
             <div className="text-center">
               <HeroTitle />
-              {/* Hero Description */}
               <HeroDescription />
-              {/* Hero Button CV */}
               <HeroButtonCV />
             </div>
           </div>
-          {/* Hero Image */}
           <HeroImage />
         </div>
       </section>
