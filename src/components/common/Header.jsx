d22b045c5d4eef4dbe69ab18c86a84b0d4991b67
Hector Clessi <121068920+hectorc2907@users.noreply.github.com>
2024-09-19 21:06:56 -0300

    configuramos un mock para el menu y una base para el nav principal

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,29 +1,47 @@
-import { useState } from "react";
 import { motion } from "framer-motion";
-import Logo from "../ui/header/Logo";
-import Menu from "../ui/header/Menu";
-// import LoginButton from "../ui/header/LoginButton";
-import MobileButton from "../ui/header/MobileButton";
-import ResponsiveMenu from "../ui/header/ResponsiveMenu";
+import Logo from "../../assets/images/logo.png";
+import LogoSmall from "../../assets/images/logoSmall.png";
+import { Menu } from "../../mocks/Menu";
 
 const Header = () => {
-  const [isOpen, setIsOpen] = useState(false);
   return (
     <>
-      <nav className="flex justify-center bg-[#1E1E1D]">
-        <motion.div
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ duration: 0.6, delay: 0.4 }}
-          className="container flex justify-between items-center py-4 md:pt-4"
-        >
-          <Logo setIsOpen={setIsOpen} />
-          <Menu />
-          {/* <LoginButton /> */}
-          <MobileButton isOpen={isOpen} setIsOpen={setIsOpen} />
-        </motion.div>
-      </nav>
-      <ResponsiveMenu isOpen={isOpen} setIsOpen={setIsOpen} />
+      <motion.nav
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 0.5 }}
+        className="bg-blue-900 p-5 flex justify-center shadow-2xl"
+      >
+        <div className="container flex justify-between items-center">
+          {/* Icon Logo Section */}
+          <motion.div
+            initial={{ opacity: 0, x: -100 }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ duration: 1, delay: 0.5 }}
+          >
+            <a href="/" className="hidden md:block">
+              <img src={Logo} alt="" className="w-64" />
+            </a>
+            <a href="/" className="md:hidden">
+              <img src={LogoSmall} alt="" className="w-[51px]" />
+            </a>
+          </motion.div>
+          {/* Menu Section */}
+          {/* Large Menu */}
+          <motion.div
+            initial={{ opacity: 0, x: 100 }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ duration: 1, delay: 0.5 }}
+            className="text-white hidden lg:flex gap-10"
+          >
+            {Menu.map((option) => (
+              <a key={option.id} href={option.link} className="text-xl font-semibold hover:text-slate-200">
+                {option.title}
+              </a>
+            ))}
+          </motion.div>
+        </div>
+      </motion.nav>
     </>
   );
 };
---
+++ src/mocks/Menu.js
@@ -0,0 +1,27 @@
+export const Menu = [
+  {
+    id: "0",
+    title: "Home",
+    link: "#home",
+  },
+  {
+    id: "1",
+    title: "Sobre Mí",
+    link: "#aboutMe",
+  },
+  {
+    id: "2",
+    title: "Proyectos",
+    link: "#projects",
+  },
+  {
+    id: "3",
+    title: "Experiencias",
+    link: "#experience",
+  },
+  {
+    id: "4",
+    title: "Contactos",
+    link: "#contact",
+  },
+];
