6273fcfcab9fb59b0af5fb8114e328e620874765
Hector Clessi <121068920+hectorc2907@users.noreply.github.com>
2024-09-20 01:03:16 -0300

    terminamos la configuracion de nuestro modelo de footer

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -1,6 +1,8 @@
 import { motion } from "framer-motion";
 import { getActualYear } from "../../utils/dateUtils";
 import Logo from "../../assets/images/logo.png";
+import { Menu } from "../../mocks/Menu";
+import { SocialMediaLinks } from "../../utils/SocialMediaLinks";
 
 const Footer = () => {
   return (
@@ -11,18 +13,46 @@ const Footer = () => {
         transition={{ duration: 0.5 }}
         className="bg-zinc-800 text-white p-5 flex justify-center"
       >
-        <div className="container flex flex-col">
+        <div className="container flex flex-col items-center lg:items-stretch">
           {/* Footer Nav Section */}
-          <div className="flex">
+          <div className="grid grid-cols-1 lg:grid-cols-3 gap-5 py-5">
             {/* Footer Logo */}
             <motion.a
               initial={{ opacity: 0, y: 50 }}
               animate={{ opacity: 1, y: 0 }}
               transition={{ duration: 1, delay: 0.5 }}
               href="/"
+              className="flex items-center"
             >
               <img src={Logo} alt="Main Logo of HAC Service" className="w-64" />
             </motion.a>
+            {/* Footer Menu */}
+            <div className="flex flex-col items-center gap-5">
+              <h5 className="text-lg font-semibold">Enlaces:</h5>
+              <div className="grid grid-cols-1 lg:grid-cols-3 xl:grid-cols-5 gap-3 lg:gap-5 text-center">
+                {Menu.map((option) => (
+                  <a
+                    key={option.id}
+                    href={option.link}
+                    className="hover:text-slate-300"
+                  >
+                    {option.title}
+                  </a>
+                ))}
+              </div>
+            </div>
+            {/* Footer Social Networks */}
+            <div className="flex flex-col items-center gap-5">
+              <h5 className="text-lg font-semibold">Redes Sociales:</h5>
+              <div className="grid grid-cols-3 lg:grid-cols-6 gap-2 text-3xl">
+                <p className="hover:text-slate-300">{SocialMediaLinks("LinkedIn")}</p>
+                <p className="hover:text-slate-300">{SocialMediaLinks("Facebook")}</p>
+                <p className="hover:text-slate-300">{SocialMediaLinks("Instagram")}</p>
+                <p className="hover:text-slate-300">{SocialMediaLinks("Twitter")}</p>
+                <p className="hover:text-slate-300">{SocialMediaLinks("WhatsApp")}</p>
+                <p className="hover:text-slate-300">{SocialMediaLinks("Telegram")}</p>
+              </div>
+            </div>
           </div>
           {/* Copyright Section */}
           <motion.div
