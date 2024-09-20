eda84d3c41cba61fecf9e022b386bdaeab281372
Hector Clessi <121068920+hectorc2907@users.noreply.github.com>
2024-09-19 23:50:08 -0300

    trabajamos en el logo del footer y sus animaciones

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -1,5 +1,6 @@
 import { motion } from "framer-motion";
 import { getActualYear } from "../../utils/dateUtils";
+import Logo from "../../assets/images/logo.png";
 
 const Footer = () => {
   return (
@@ -11,7 +12,19 @@ const Footer = () => {
         className="bg-zinc-800 text-white p-5 flex justify-center"
       >
         <div className="container flex flex-col">
-          <div className="flex">a</div>
+          {/* Footer Nav Section */}
+          <div className="flex">
+            {/* Footer Logo */}
+            <motion.a
+              initial={{ opacity: 0, y: 50 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 1, delay: 0.5 }}
+              href="/"
+            >
+              <img src={Logo} alt="Main Logo of HAC Service" className="w-64" />
+            </motion.a>
+          </div>
+          {/* Copyright Section */}
           <div className="pt-5 text-center flex flex-col">
             <p>HAC Software {getActualYear()}</p>
             <p>&copy; Todos los Derechos Reservados</p>
