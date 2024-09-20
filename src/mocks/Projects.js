8f5eed8c7c6662932a18814ad8c6082533a64f1f
Hector Clessi <121068920+hectorc2907@users.noreply.github.com>
2024-09-20 17:39:16 -0300

    creamos un mock para los proyectos

---
+++ src/mocks/Projects.js
@@ -0,0 +1,12 @@
+export const Projects = [
+  {
+    id: 0,
+    title: "Portfolio Web",
+    type: "Front-End",
+    description: "lorem ipsum",
+    technologies: [""],
+    repository: "",
+    preview: "",
+    img: "",
+  },
+];
