804309309f84c81df3ed61962afaf66bbb9e02b2
Hector Clessi <121068920+hectorc2907@users.noreply.github.com>
2024-09-22 23:20:57 -0300

    agregamos animaciones a la seccion de resumen

--- src/components/ui/Resume/Resume.jsx
+++ src/components/ui/Resume/Resume.jsx
@@ -1,53 +1,95 @@
 import { MyEducation } from "../../../mocks/MyEducation";
 import { MyExperience } from "../../../mocks/MyExperience";
+import { motion } from "framer-motion";
+import { MdKeyboardDoubleArrowUp } from "react-icons/md";
 
 const Resume = () => {
   return (
     <>
-      <section className="bg-blue-900 md:bg-white px-5 py-10 flex justify-center text-white">
-        <div className="bg-blue-900 container grid grid-cols-1 gap-10 rounded-2xl p-5">
-          <h2 className="text-4xl italic font-semibold text-center">
+      <motion.section
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 1 }}
+        className="bg-blue-900 md:bg-white px-5 py-10 flex justify-center text-white"
+      >
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 1 }}
+          className="bg-blue-900 md:shadow-md md:shadow-slate-900 container grid grid-cols-1 gap-10 rounded-2xl p-5"
+        >
+          <motion.h2
+            initial={{ opacity: 0, y: -100 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 1, delay: 0.5 }}
+            className="text-4xl italic font-semibold text-center"
+          >
             Trayectoria
-          </h2>
+          </motion.h2>
           <div className="grid grid-cols-1 gap-14">
             <div className="grid grid-cols-1 gap-5">
-              <h3 className="text-center text-2xl italic">
+              <motion.h3
+                initial={{ opacity: 0, x: 100 }}
+                animate={{ opacity: 1, x: 0 }}
+                transition={{ duration: 1, delay: 0.5 }}
+                className="text-center text-2xl italic"
+              >
                 Experiencia Profesional
-              </h3>
+              </motion.h3>
               <div className="grid grid-cols-1 lg:grid-cols-2 gap-5 text-black">
-                {[...MyExperience].reverse().map((experience) => (
-                  <div className="grid grid-cols-1 bg-white rounded-2xl px-5 shadow-md shadow-slate-900">
+                {[...MyExperience].reverse().map((experience, index) => (
+                  <motion.div
+                    initial={{ opacity: 0, y: 50 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    transition={{ duration: 1, delay: 0.5 + 0.2 * index }}
+                    className="grid grid-cols-1 bg-white rounded-2xl px-5 shadow-md shadow-slate-900"
+                  >
                     <p className="text-center text-xl font-semibold">
                       {experience.position}
                     </p>
                     <p className="text-center">
-                      Trayectoria: {experience.timeline}
+                      Duración: {experience.timeline}
                     </p>
                     <p className="text-center py-5">{experience.description}</p>
                     <p>Compañia: {experience.company}</p>
-                  </div>
+                  </motion.div>
                 ))}
               </div>
             </div>
             <div className="grid grid-cols-1 gap-5">
-              <h3 className="text-center text-2xl italic">Formación</h3>
+              <motion.h3
+                initial={{ opacity: 0, x: -100 }}
+                animate={{ opacity: 1, x: 0 }}
+                transition={{ duration: 1, delay: 0.5 }}
+                className="text-center text-2xl italic"
+              >
+                Formación
+              </motion.h3>
               <div className="grid grid-cols-1 lg:grid-cols-2 gap-5 text-black">
-                {[...MyEducation].reverse().map((education) => (
-                  <div className="grid grid-cols-1 bg-white rounded-2xl px-5 shadow-md shadow-slate-900">
-                    <a href={education.link}>
+                {[...MyEducation].reverse().map((education, index) => (
+                  <motion.div
+                    initial={{ opacity: 0, y: 50 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    transition={{ duration: 1, delay: 0.5 + 0.2 * index }}
+                    className="grid grid-cols-1 bg-white rounded-2xl px-5 shadow-md shadow-slate-900"
+                  >
+                    <a href={education.link} target="_blank">
                       <p className="text-center text-xl font-semibold">
                         {education.title}
                       </p>
                     </a>
+                    <p className="text-slate-500 flex justify-center items-center">
+                      <MdKeyboardDoubleArrowUp /> <span>Certificado</span>
+                    </p>
                     <p className="text-center py-5">{education.description}</p>
                     <p>Institución: {education.institution}</p>
-                  </div>
+                  </motion.div>
                 ))}
               </div>
             </div>
           </div>
-        </div>
-      </section>
+        </motion.div>
+      </motion.section>
     </>
   );
 };
