96a8232cec00da10a47f6c05876c6cf64890c96a
Hector Clessi <121068920+hectorc2907@users.noreply.github.com>
2024-09-22 23:10:55 -0300

    concluimos con la parte de formación

--- src/components/ui/Resume/Resume.jsx
+++ src/components/ui/Resume/Resume.jsx
@@ -1,3 +1,4 @@
+import { MyEducation } from "../../../mocks/MyEducation";
 import { MyExperience } from "../../../mocks/MyExperience";
 
 const Resume = () => {
@@ -8,13 +9,13 @@ const Resume = () => {
           <h2 className="text-4xl italic font-semibold text-center">
             Trayectoria
           </h2>
-          <div className="grid grid-cols-1 lg:grid-cols-2">
+          <div className="grid grid-cols-1 gap-14">
             <div className="grid grid-cols-1 gap-5">
               <h3 className="text-center text-2xl italic">
                 Experiencia Profesional
               </h3>
-              <div className="grid grid-cols-1 gap-5 text-black">
-                {MyExperience.reverse().map((experience) => (
+              <div className="grid grid-cols-1 lg:grid-cols-2 gap-5 text-black">
+                {[...MyExperience].reverse().map((experience) => (
                   <div className="grid grid-cols-1 bg-white rounded-2xl px-5 shadow-md shadow-slate-900">
                     <p className="text-center text-xl font-semibold">
                       {experience.position}
@@ -28,7 +29,22 @@ const Resume = () => {
                 ))}
               </div>
             </div>
-            <div className="grid grid-cols-1"></div>
+            <div className="grid grid-cols-1 gap-5">
+              <h3 className="text-center text-2xl italic">Formación</h3>
+              <div className="grid grid-cols-1 lg:grid-cols-2 gap-5 text-black">
+                {[...MyEducation].reverse().map((education) => (
+                  <div className="grid grid-cols-1 bg-white rounded-2xl px-5 shadow-md shadow-slate-900">
+                    <a href={education.link}>
+                      <p className="text-center text-xl font-semibold">
+                        {education.title}
+                      </p>
+                    </a>
+                    <p className="text-center py-5">{education.description}</p>
+                    <p>Institución: {education.institution}</p>
+                  </div>
+                ))}
+              </div>
+            </div>
           </div>
         </div>
       </section>
