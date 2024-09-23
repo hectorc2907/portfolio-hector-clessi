0c646e77ca2320e6240fb7f952044dd5c649e46f
Hector Clessi <121068920+hectorc2907@users.noreply.github.com>
2024-09-22 22:14:03 -0300

    llenamos las barras de experiencias

--- src/mocks/MyExperience.js
+++ src/mocks/MyExperience.js
@@ -1,3 +1,41 @@
-export const MyExperience = [{
-    
-}];
+export const MyExperience = [
+  {
+    id: 0,
+    position: "Agente Telefonico",
+    task: [
+      "Atención al cliente y resolución de problemas.",
+      "Gestión de consultas y reclamaciones de usuarios.",
+      "Manejo de CRM para seguimiento de casos.",
+      "Cumplimiento de métricas de calidad y eficiencia.",
+      "Resolución bajo presión.",
+    ],
+    timeline: "01-2018 | 07-2023",
+    company: "Teleperformance",
+  },
+  {
+    id: 1,
+    position: "Full-Stack Developer",
+    task: [
+      "Desarrollo de aplicaciones web utilizando el stack MERN (MongoDB, Express.js, React, Node.js).",
+      "Implementación de API REST para interacción cliente-servidor.",
+      "Diseño y desarrollo de interfaces de usuario con React.",
+      "Integración de bases de datos NoSQL (MongoDB).",
+      "Deploy Front-End en Netlify y Back-End en Vercel.",
+    ],
+    timeline: "11-2022 | 08-2023",
+    company: "RollingCode Schools",
+  },
+  {
+    id: 2,
+    position: "Freelance",
+    task: [
+      "Desarrollo de proyectos web personalizados con el stack MERN.",
+      "Mantenimiento y optimización de aplicaciones existentes.",
+      "Consultoría tecnológica y recomendaciones técnicas.",
+      "Diseño e implementación de soluciones escalables y eficientes.",
+      "Deploy en OnRender tanto Front, como Back.",
+    ],
+    timeline: "09-2023 | Actualidad",
+    company: "Independiente",
+  },
+];
